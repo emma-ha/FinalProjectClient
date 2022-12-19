57421cbfd8bc310fb24ef004f2e355e006ffaf23
Emma Haque <emmaha299@gmail.com>
2022-12-19 00:05:04 -0500

    modified some files

--- src/components/views/CampusView.js
+++ src/components/views/CampusView.js
@@ -1,6 +1,5 @@
 /*==================================================
 CampusView.js
-
 The Views component is responsible for rendering web page with data provided by the corresponding Container component.
 It constructs a React component to display a single campus and its students (if any).
 ================================================== */
